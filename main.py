2478059569fc681ce44c460f36fdcd93113baacb
ArunaKale22 <yellayiaruna@gmail.com>
2025-03-12 17:59:59 +0530

    Added the new files like musicLibrary.py and client.py

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
+import requests
+import os
+import google.generativeai as genai
+
+
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+newsapi = "92b6702695f14a61b761ea3d036a372f"
+genai.configure(api_key="AIzaSyAFCb40p1hJvLRoihkPJ2qHIBICqOQ4X44")
+
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def aiProcess(command):
+    model = genai.GenerativeModel("gemini-1.5-flash")  # Use correct model name
+    response = model.generate_content(command)
+    return response.text
+    # print(response.text)  # Print the AI-generated response
+    # generate()
+
 def processCommand(c):
-    pass
+    if "open google" in c.lower():
+        webbrowser.open("https://www.google.com")
+
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://www.facebook.com")
+
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com")
+
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://www.linkedin.com")
     
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+    
+    elif "news" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={newsapi}")
+        if r.status_code == 200:
+            # Parse the JSON response
+            data = r.json()
+            # Extract articles
+            articles = data.get("articles", [])
+            # Speakout the headlines
+            for article in articles:
+                speak(article['title'])
+    else:
+        # Let OpenAI handle the request
+        output = aiProcess(c)
+        speak(output)
+        pass   
+        
+
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
 
@@ -34,7 +82,7 @@ if __name__ == "__main__":
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
 
-                    processCommand()
+                    processCommand(command)
         except Exception as e:
             print("Error; {0}".format(e))
 
---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "niceMorning": "https://www.youtube.com/watch?v=5O118dW7zbc",
+    "traveller": "https://www.youtube.com/watch?v=D_vAt1r1_VA&list=OLAK5uy_mlkqnqZOGyM2V-wvmEp8lDT23H6-EixM8",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D"
+}
